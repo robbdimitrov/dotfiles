c455ce26d81b54e06dcc22772987e3f09b3113e6
Robert Dimitrov <robert_dimitrov@me.com>
2026-06-24 19:15:09 +0300

    refactor: consolidate exports and use zsh path arrays
    - Cleaned up .zshrc to be a pure entrypoint
    - Moved all ENV variables to .zsh/exports.zsh
    - Migrated from manual string path concatenation to Zsh native arrays

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,7 +1,28 @@
-# Environment variable exports
-
+# ==============================================================================
+# 1. CORE EXPORTS
+# ==============================================================================
+export LANG=en_US.UTF-8
+export EDITOR=vim
+export GOPATH=$HOME/.go
 export LESS='-R'
 export PAGER='less'
 
 # Exclude . - / from word characters to behave like macOS terminal when deleting words
-WORDCHARS=${WORDCHARS//[\.\-\/]/}
+export WORDCHARS=${WORDCHARS//[\.\-\/]/}
+
+# ==============================================================================
+# 2. PATH MANAGEMENT
+# ==============================================================================
+# Remove duplicates from the path array
+typeset -U path
+
+# Add directories to your path cleanly (top-down priority)
+path=(
+  /opt/homebrew/bin
+  /opt/homebrew/opt/libpq/bin
+  $HOME/.cargo/bin
+  $HOME/.local/bin
+  $HOME/.go/bin
+  $path
+)
+export PATH
--- .zshrc
+++ .zshrc
@@ -1,21 +1,5 @@
-# Shell
-export LANG=en_US.UTF-8
+# ==============================================================================
+# ZSH CONFIGURATION ENTRYPOINT
+# ==============================================================================
+
 source ~/.zsh/setup.zsh
-
-# User
-export EDITOR=vim
-
-# Brew
-export PATH=/opt/homebrew/bin:$PATH
-
-# Golang
-export GOPATH=$HOME/.go
-
-# Tools
-export PATH=/opt/homebrew/opt/libpq/bin:$PATH
-
-# Harnesses
-export PATH=$HOME/.local/bin:$PATH
-
-# Rust
-export PATH=$HOME/.cargo/bin:$PATH
